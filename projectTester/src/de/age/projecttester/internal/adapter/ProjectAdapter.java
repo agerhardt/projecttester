064ada89ac3936a995f333e26c9ca3c5ef154e64
Alexander Gerhardt <h.a.gerhardt@googlemail.com>
2011-09-24 19:15:25 +0200

    add some impl to the projectadapter

--- projectTester/src/de/age/projecttester/internal/adapter/ProjectAdapter.java
+++ projectTester/src/de/age/projecttester/internal/adapter/ProjectAdapter.java
@@ -1,6 +1,9 @@
 package de.age.projecttester.internal.adapter;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.junit.JUnitCore;
 
 import de.age.projecttester.internal.Project;
 
@@ -13,13 +16,25 @@ public class ProjectAdapter implements Project {
 	
 	public ProjectAdapter(IJavaProject project) {
 		super();
+		if (project == null) {
+			throw new NullPointerException("Wrapped project must not be <null>.");
+		}
 		this.project = project;
 	}
 
 	@Override
 	public String[] getTestableClassnames() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			IType[] testTypes = JUnitCore.findTestTypes(project, null);
+			String[] result = new String[testTypes.length];
+			for (int i = 0; i < testTypes.length; i++) {
+				result[i] = testTypes[i].getFullyQualifiedName();
+			}
+			return result;
+		} catch (CoreException e) {
+			// TODO
+			return null;
+		}
 	}
 
 	@Override
