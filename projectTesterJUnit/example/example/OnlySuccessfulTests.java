58e29c417f79e9262ccc8d7fd7761c16cb2fdace
Alexander Gerhardt <h.a.gerhardt@googlemail.com>
2011-09-27 14:21:33 +0200

    another class for testing only

---
+++ projectTesterJUnit/example/example/OnlySuccessfulTests.java
@@ -0,0 +1,18 @@
+package example;
+
+import org.junit.Test;
+
+/**
+ * DO NOT USE, JUST FOR TESTING PURPOSES.
+ */
+public class OnlySuccessfulTests {
+
+	@Test
+	public void successful1() {
+	}
+
+	@Test
+	public void successful2() {
+	}
+
+}
