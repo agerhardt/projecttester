b731208557128f38af1b44bf8e7cdfab434d32a7
Alexander Gerhardt <h.a.gerhardt@googlemail.com>
2011-09-27 12:05:37 +0200

    new testcase

--- projectTesterJUnit/src/de/age/projecttester/internal/adapter/ProjectSuiteTest.java
+++ projectTesterJUnit/src/de/age/projecttester/internal/adapter/ProjectSuiteTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 
 import org.easymock.EasyMock;
 import org.junit.Test;
+import org.junit.internal.builders.AllDefaultPossibilitiesBuilder;
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.RunnerBuilder;
 
@@ -15,11 +16,9 @@ public class ProjectSuiteTest {
 	@Test
 	public void descriptionReturnsNameOfProject() throws InitializationError {
 		String projectName = "project1";
-		String className1 = "className1";
-		String className2 = "className2";
 		
 		Project project = EasyMock.createMock(Project.class);
-		EasyMock.expect(project.getTestableClassnames()).andStubReturn(new String[] { className1, className2} );
+		EasyMock.expect(project.getTestableClassnames()).andStubReturn(new String[0]);
 		EasyMock.expect(project.getDependantProjects()).andStubReturn(new Project[0]);
 		EasyMock.expect(project.getName()).andStubReturn(projectName);
 		
@@ -29,5 +28,23 @@ public class ProjectSuiteTest {
 		
 		ProjectSuite suite = new ProjectSuite(project, builder, new Class<?>[0]);
 		assertThat(suite.getDescription().getDisplayName(), is(projectName));
+	}
+	
+	@Test
+	public void descriptionReturnsTestClasses() throws InitializationError, ClassNotFoundException {
+		String projectName = "project1";
+		String className1 = "example.ExampleJUnit4Class";
+		
+		Project project = EasyMock.createMock(Project.class);
+		EasyMock.expect(project.getTestableClassnames()).andStubReturn(new String[] { className1 } );
+		EasyMock.expect(project.getDependantProjects()).andStubReturn(new Project[0]);
+		EasyMock.expect(project.getName()).andStubReturn(projectName);
+		
+		EasyMock.replay(project);
+		
+		AllDefaultPossibilitiesBuilder runnerBuilder = new AllDefaultPossibilitiesBuilder(true);
+		
+		ProjectSuite suite = new ProjectSuite(project, runnerBuilder, new Class<?>[] { Class.forName(className1) });
+		assertThat(suite.getDescription().getChildren().size(), is(1));
 	}
 }
